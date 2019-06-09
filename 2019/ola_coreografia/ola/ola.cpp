d09a57c1b4504a5ba88cd896813d2ba6e740f9b0
everttonbs <ebs@ic.ufal.br>
2019-06-09 01:05:23 -0300

    Coreografia da Ola

--- 2019/ola_coreografia/ola/ola.cpp
+++ 2019/ola_coreografia/ola/ola.cpp
@@ -4,8 +4,17 @@
 #include <stdlib.h>
 #include <iostream>
 
-static float y = 150.0f;
-static float z = 150.0f;
+static float x = 75.0f;
+static float y = 15.0f;
+static float y0 = -15.0f;
+static float y1 = 15.0f;
+static float y2 = 15.0f;
+static float y3 = 15.0f;
+static float y4 = 15.0f;
+static float y5 = 15.0f;
+static float y6 = 15.0f;
+static float y7 = 15.0f;
+static float z = 50.0f;
 
 using namespace std;
 
@@ -25,38 +34,105 @@ void display(void)
     glBegin(GL_QUADS);
 
         glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f(x, y1 + z);
+        glVertex2f(x, y1);
+        glVertex2f(x + z, y1);
+        glVertex2f(x + z , y1 + z);
 
-        glVertex2f(0.0f, 50.0f);
-        glVertex2f(100.0f, 50.0f);
-        glVertex2f(100.0f, y);
-        glVertex2f(0.0f, y);
+
+        glColor3f(0.0f, 1.0f, 1.0f);
+        glVertex2f(x + 50, y2 + z);
+        glVertex2f(x + 50, y2);
+        glVertex2f(x + 50 + z, y2);
+        glVertex2f(x + 50 + z , y2 + z);
+
 
         glColor3f(1.0f, 1.0f, 0.0f);
-
-        glVertex2f(120.0f, 50.0f);
-        glVertex2f(220.0f, 50.0f);
-        glVertex2f(220.0f, y);
-        glVertex2f(120.0f, y);
+        glVertex2f(x + 100, y3 + z);
+        glVertex2f(x + 100, y3);
+        glVertex2f(x + 100 + z, y3);
+        glVertex2f(x + 100 + z , y3 + z);
 
 
         glColor3f(1.0f, 0.0f, 0.0f);
-
-        glVertex2f(240.0f, 50.0f);
-        glVertex2f(340.0f, 50.0f);
-        glVertex2f(340.0f, y);
-        glVertex2f(240.0f, y);
+        glVertex2f(x + 150, y4 + z);
+        glVertex2f(x + 150, y4);
+        glVertex2f(x + 150 + z, y4);
+        glVertex2f(x + 150 + z , y4 + z);
 
         glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(x + 200, y5 + z);
+        glVertex2f(x + 200, y5);
+        glVertex2f(x + 200 + z, y5);
+        glVertex2f(x + 200 + z , y5 + z);
 
-        glVertex2f(360.0f, 50.0f);
-        glVertex2f(460.0f, 50.0f);
-        glVertex2f(460.0f, y);
-        glVertex2f(360.0f, y);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(x + 250, y6 + z);
+        glVertex2f(x + 250, y6);
+        glVertex2f(x + 250 + z, y6);
+        glVertex2f(x + 250 + z , y6 + z);
+
+        glColor3f(1.0f, 1.0f, 0.50f);
+        glVertex2f(x + 300, y7 + z);
+        glVertex2f(x + 300, y7);
+        glVertex2f(x + 300 + z, y7);
+        glVertex2f(x + 300 + z , y7 + z);
+
+
 
     glEnd();
 
     glFlush();
     glutSwapBuffers();
+
+}
+
+void Up(int value)
+{
+    y0 += 5;
+
+    if(y0 > 0 && y1 < 350)
+        y1 += 5;
+    else
+        y1 = y;
+
+    if(y0 > 50 && y2 < 350)
+        y2+= 5;
+    else
+        y2 = y;
+
+    if(y0 > 100 && y3 < 350)
+        y3+=5;
+    else
+        y3 = y;
+
+    if(y0 > 150 && y4 < 350)
+        y4+= 5;
+    else
+        y4 = y;
+
+    if(y0 > 200 && y5 < 350)
+        y5+= 5;
+    else
+        y5 = y;
+
+    if(y0 > 250 && y6 < 350)
+        y6+= 5;
+    else
+        y6 = y;
+
+    if(y0 > 300 && y7 < 350)
+        y7+= 5;
+    else
+        y7 = y;
+
+
+    glutPostRedisplay();
+    glutTimerFunc(30, Up, 1);
+
+
+
+
 
 }
 
@@ -66,17 +142,25 @@ void GerenciaTeclado(unsigned char key, int x1, int y1)
     switch(key)
     {
     case 'o':
-        y = 250.0f;
+        //y = 250.0f;
+        //x += 5;
+        //y += 5;
+        glutTimerFunc(33, Up, 1);
+
+
         break;
 
     case 'p':
-        y = 150.0f;
+        y -= 5;
         break;
 
     }
     glutPostRedisplay();
 
 }
+
+
+
 
 int main(int argc, char** argv){
 
@@ -85,6 +169,8 @@ int main(int argc, char** argv){
     glutInitWindowSize (500, 500);
     glutInitWindowPosition (200, 200);
     glutCreateWindow ("Ola_Torcida");
+
+    //glutTimerFunc(33, Timer, 1);
 
     init();
     glutDisplayFunc(display);
